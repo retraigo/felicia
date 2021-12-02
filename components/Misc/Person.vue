dd7cf868c729abc28855427b9078d5e35d7e002f
Caligo <schattenetcaligo@gmail.com>
2021-12-02 21:22:40 +0530

    New markdown component for people info

---
+++ components/Misc/Person.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="p-2 flex flex-col justify-start items-start space-y-2 w-full">
+    <p class = "text-lg font-semibold">{{news.title}}</p>
+        <div class = "text-xs text-gray-700">{{news.description}}</div>
+
+      <nuxt-content
+        class="prose prose-sm"
+        :document="news"
+      />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    news: {
+      type: Object,
+      default: null,
+    },
+  },
+};
+</script>
