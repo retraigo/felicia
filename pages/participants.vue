cae1d184473c01b688e1a3779f82971faaa16a21
Caligo <schattenetcaligo@gmail.com>
2021-12-15 00:05:40 +0530

    redirect

---
+++ pages/participants.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <p class = "text-white font-semibold text-center text-4xl animate-pulse">Redirecting you...</p>
+</div>
+</template>
+
+<script>
+export default {
+  mounted () {
+         setTimeout(function(){
+        window.location.href = 'https://easychair.org/conferences/?conf=ivcmasm2022' 
+         }, 3000)
+  }
+}
+</script>
+
